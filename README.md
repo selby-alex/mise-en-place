9b7acb1768e0b333f7374604e21a6364418e1bcb
Alex <alxselby@gmail.com>
2022-04-11 12:41:09 -0400

    adding postgres integration and scaffolding

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # mise-en-place
 Cookbook for general code snippets
+
+
+
+# Virtual env + package installation
+set up a virtual enviroment and install required packages
+`python3 pip install virtualenv`
+`virtualenv venv`
+`source venv/bin/activate`
+`pip install -r requirements.txt`
+
+
+don't forget to add a .env file!
---
+++ database.py
@@ -0,0 +1,45 @@
+import psycopg2 as pg
+import os
+from dotenv import find_dotenv, load_dotenv
+
+
+load_dotenv(find_dotenv())
+
+
+class Database:
+    def __init__(self):
+        self.connect()
+
+    def connect(self):
+        self.conn = pg.connect(
+            user=os.environ["DB_USER"],
+            password=os.environ.get("DB_PW"),
+            host=os.environ["DB_HOST"],
+            port=os.environ["DB_PORT"],
+            database=os.environ["DB_NAME"],
+        )
+        self.curr = self.conn.cursor()
+
+    def get_query(self, query_name):
+        query = open(f"./SQL/{query_name}", "r")
+        return query.read()
+
+    def execute_query(self, query=None, query_name=None, filters=None):
+        if query_name:
+            query = self.get_query(query_name)
+
+        if filters:
+            query += f"\n {filters}"
+
+        if query:
+            self.curr.execute(query)
+            records = self.curr.fetchall()
+            return records
+
+        return None
+
+
+if __name__ == "__main__":
+    ## example usage
+    db = Database()
+    records = db.execute_query(query="sample_query.sql", filters="WHERE 1=1")
